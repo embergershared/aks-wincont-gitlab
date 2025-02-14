d221d0ddf3c28086f78be50a54a9ce8886b6a11d
Emmanuel <bergerat62@gmail.com>
2025-02-14 11:11:01 -0500

    windows nodepool fix

--- azure/terraform/aks-lz-accelerator/06-AKS-Cluster/main.tf
+++ azure/terraform/aks-lz-accelerator/06-AKS-Cluster/main.tf
@@ -174,6 +174,7 @@ resource "azurerm_kubernetes_cluster_node_pool" "nodepool" {
   kubernetes_cluster_id = azurerm_kubernetes_cluster.aks-cluster.id
   vm_size               = "Standard_DS2_v2"
   os_disk_size_gb       = 30
+  os_type               = "Linux"
   os_sku                = "Ubuntu"
   min_count             = 1
   max_count             = 3
@@ -189,6 +190,7 @@ resource "azurerm_kubernetes_cluster_node_pool" "win_nodepool" {
   kubernetes_cluster_id = azurerm_kubernetes_cluster.aks-cluster.id
   vm_size               = "Standard_DS2_v2"
   os_disk_size_gb       = 30
+  os_type               = "Windows"
   os_sku                = "Windows2022"
   min_count             = 1
   max_count             = 3

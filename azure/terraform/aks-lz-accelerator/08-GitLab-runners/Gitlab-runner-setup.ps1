6900d904a4ad061da68ba2ffd15bead7d6cb8aaf
Emmanuel <bergerat62@gmail.com>
2025-02-26 15:32:36 -0500

    Updated GitLab runner script

--- azure/terraform/aks-lz-accelerator/08-GitLab-runners/Gitlab-runner-setup.ps1
+++ azure/terraform/aks-lz-accelerator/08-GitLab-runners/Gitlab-runner-setup.ps1
@@ -1,9 +1,9 @@
-# 1. Install Chocolatey
+# Install Chocolatey
 Set-ExecutionPolicy Bypass -Scope Process -Force
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
-# 2. Chocolatey functions
+# Chocolatey functions
 Function Install-ChocoPackage {
   param (
     [Parameter(Mandatory = $true)]
@@ -18,6 +18,7 @@ Function Install-ChocoPackage {
   }
 }
 
+# Install core packages
 $gl_runner_packages = @(
   "git",
   "powershell-core",
@@ -58,5 +59,39 @@ $gl_runner_packages = @(
   # "vscode",
   # "winscp"
 )
-
 Install-ChocoPackage -Packages $gl_runner_packages
+
+# Set VM time zone
+Set-TimeZone -Name "Eastern Standard Time"
+
+# Install Docker for Windows containers
+## Optionally enable required Windows features if needed
+
+Enable-WindowsOptionalFeature -Online -FeatureName containers –All
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V –All
+
+curl.exe -o docker.zip -LO https://download.docker.com/win/static/stable/x86_64/docker-20.10.13.zip 
+Expand-Archive docker.zip -DestinationPath C:\
+[Environment]::SetEnvironmentVariable("Path", "$($env:path);C:\docker", [System.EnvironmentVariableTarget]::Machine)
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine")
+dockerd --register-service
+Start-Service docker
+docker run hello-world
+
+
+# Install GitLab Runner
+
+New-Item -Path 'C:\GitLab-Runner' -ItemType Directory
+cd 'C:\GitLab-Runner'
+
+## Download binary
+Invoke-WebRequest -Uri "https://gitlab-runner-downloads.s3.amazonaws.com/latest/binaries/gitlab-runner-windows-amd64.exe" -OutFile "gitlab-runner.exe"
+
+## Register the runner (steps below), then run
+.\gitlab-runner.exe install
+.\gitlab-runner.exe start
+
+# Register GitLab Runner
+$TOKEN = ""
+.\gitlab-runner.exe register  --url https://gitlab.com  --token $TOKEN
+

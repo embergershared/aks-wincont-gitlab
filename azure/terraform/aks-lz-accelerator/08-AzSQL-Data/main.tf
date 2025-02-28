73e32406964d7ba5629ba28c55f28d91974d106f
Emmanuel <bergerat62@gmail.com>
2025-02-28 13:07:05 -0500

    Added private endpoint to POC storage account

--- azure/terraform/aks-lz-accelerator/08-AzSQL-Data/main.tf
+++ azure/terraform/aks-lz-accelerator/08-AzSQL-Data/main.tf
@@ -123,4 +123,65 @@ resource "azurerm_storage_share" "this" {
   quota              = 1024
   storage_account_id = azurerm_storage_account.this.id
 }
+
+################################  Private endpoints for Storage Account  ################################
+/*
+resource "azurerm_private_endpoint" "pe_blob" {
+  name                          = "pe-to-blob-${replace(azurerm_storage_account.this.name, "-", "")}"
+  resource_group_name           = var.rgLzName
+  location                      = var.location
+  subnet_id                     = local.speSubnetId
+  custom_network_interface_name = "nic-pe-to-blob-${replace(azurerm_storage_account.this.name, "-", "")}"
+
+  private_service_connection {
+    name                           = "${var.vnetLzName}_${data.azurerm_subnet.snet-spe.0.name}_connection"
+    private_connection_resource_id = azurerm_storage_account.this.id
+    subresource_names              = ["blob"]
+    is_manual_connection           = true
+    request_message                = "Please approve this PE connection to ${var.vnetLzName}/${data.azurerm_subnet.snet-spe.0.name}."
+  }
+
+  tags = merge(var.base_tags, var.plan_tags)
+}
+#     / Create Private DNS records for Private Endpoint
+resource "azurerm_private_dns_a_record" "pe_blob_dns_record" {
+  name                = azurerm_storage_account.this.name
+  zone_name           = "privatelink.blob.core.windows.net"
+  resource_group_name = var.rgLzName
+  ttl                 = 30
+  records             = [azurerm_private_endpoint.pe_blob.private_service_connection[0].private_ip_address]
+
+  lifecycle { ignore_changes = [tags["creator"]] }
+  tags = merge(var.base_tags, var.plan_tags)
+}
+#*/
+
+resource "azurerm_private_endpoint" "pe_file" {
+  name                          = "pe-to-file-${replace(azurerm_storage_account.this.name, "-", "")}"
+  resource_group_name           = var.rgLzName
+  location                      = var.location
+  subnet_id                     = local.speSubnetId
+  custom_network_interface_name = "nic-pe-to-file-${replace(azurerm_storage_account.this.name, "-", "")}"
+
+  private_service_connection {
+    name                           = "${var.vnetLzName}_${data.azurerm_subnet.snet-spe.0.name}_connection"
+    private_connection_resource_id = azurerm_storage_account.this.id
+    subresource_names              = ["file"]
+    is_manual_connection           = true
+    request_message                = "Please approve this PE connection to ${var.vnetLzName}/${data.azurerm_subnet.snet-spe.0.name}."
+  }
+
+  tags = merge(var.base_tags, var.plan_tags)
+}
+#     / Create Private DNS records for Private Endpoint
+resource "azurerm_private_dns_a_record" "pe_file_dns_record" {
+  name                = azurerm_storage_account.this.name
+  zone_name           = "privatelink.file.core.windows.net"
+  resource_group_name = var.rgLzName
+  ttl                 = 30
+  records             = [azurerm_private_endpoint.pe_file.private_service_connection[0].private_ip_address]
+
+  lifecycle { ignore_changes = [tags["creator"]] }
+  tags = merge(var.base_tags, var.plan_tags)
+}
 #*/

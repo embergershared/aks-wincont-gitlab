ee535ce422d424c1fc6509c6e3ecae29985120d8
Emmanuel <bergerat62@gmail.com>
2025-03-03 13:26:23 -0500

    Fixed spaces in $PATH

--- azure/terraform/aks-lz-accelerator/09-GitLab-runner/Gitlab-runner-setup.ps1
+++ azure/terraform/aks-lz-accelerator/09-GitLab-runner/Gitlab-runner-setup.ps1
@@ -117,23 +117,26 @@ $TOKEN = $GitLabRunnerToken
 Add-Content -Path $filePath -Value "$(Get-Date): GitLab Runner installed"
 
 # Add additional entries to the PATH
-[Environment]::SetEnvironmentVariable("Path", "$($env:path); C:\Program Files\Git\bin; C:\Program Files\Microsoft VS Code\bin; C:\Program Files\Microsoft SDKs\Azure\CLI2\wbin; C:\Program Files\PowerShell\7; C:\Program Files\SqlCmd", [System.EnvironmentVariableTarget]::Machine)
+[Environment]::SetEnvironmentVariable("Path", "$($env:path);C:\Program Files\Git\bin;C:\Program Files\Microsoft VS Code\bin;C:\Program Files\Microsoft SDKs\Azure\CLI2\wbin;C:\Program Files\PowerShell\7;C:\Program Files\SqlCmd", [System.EnvironmentVariableTarget]::Machine)
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine")
 Add-Content -Path $filePath -Value "$(Get-Date): Added paths to the PATH environment variable"
 
-# Mount the Azure File Share as Z drive on the GitLab runner VM
-$connectTestResult = Test-NetConnection -ComputerName "$StAcctName.file.core.windows.net" -Port 445
-if ($connectTestResult.TcpTestSucceeded) {
-  # Save the password so the drive will persist on reboot
-  cmd.exe /C "cmdkey /add:`"$StAcctName.file.core.windows.net`" /user:`"localhost\$StAcctName`" /pass:`"$StAcctAccessKey`""
-  # Mount the drive
-  New-PSDrive -Name Z -PSProvider FileSystem -Root "\\$StAcctName.file.core.windows.net\$StShareName" -Persist
-  Add-Content -Path $filePath -Value "$(Get-Date): Mounted Azure File Share as Z drive"
-}
-else {
-  Write-Error -Message "Unable to reach the Azure storage account via port 445. Check to make sure your organization or ISP is not blocking port 445, or use Azure P2S VPN, Azure S2S VPN, or Express Route to tunnel SMB traffic over a different port."
-  Add-Content -Path $filePath -Value "$(Get-Date): Unable to reach the Azure storage account via port 445"
-}
+# # Mount the Azure File Share as Z drive on the GitLab runner VM
+# $connectTestResult = Test-NetConnection -ComputerName "$StAcctName.file.core.windows.net" -Port 445
+# if ($connectTestResult.TcpTestSucceeded) {
+#   # Save the password so the drive will persist on reboot
+#   cmd.exe /C "cmdkey /add:`"$StAcctName.file.core.windows.net`" /user:`"localhost\$StAcctName`" /pass:`"$StAcctAccessKey`""
+#   # Mount the drive
+#   New-PSDrive -Name Z -PSProvider FileSystem -Root "\\$StAcctName.file.core.windows.net\$StShareName" -Persist
+#   Get-ChildItem Z:\ | Out-File -FilePath $filePath
+#   Add-Content -Path $filePath -Value "$(Get-Date): Mounted Azure File Share as Z drive"
+# }
+# else {
+#   Write-Error -Message "Unable to reach the Azure storage account via port 445. Check to make sure your organization or ISP is not blocking port 445, or use Azure P2S VPN, Azure S2S VPN, or Express Route to tunnel SMB traffic over a different port."
+#   Add-Content -Path $filePath -Value "$(Get-Date): Unable to reach the Azure storage account via port 445"
+# }
+
+# TODO: add code to retrieve BACPAC files from Azure Blob with managed identity + Private endpoint
 
 # Restart the machine
 Add-Content -Path $filePath -Value "$(Get-Date): Launching Server restart"

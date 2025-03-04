94f7b638481a60ec66f5f131a52f66722829e209
Emmanuel <bergerat62@gmail.com>
2025-03-03 23:12:14 -0500

    Removed 10-GitLab-self-hosted

--- azure/terraform/aks-lz-accelerator/10-GitLab-Self-managed/10_common_values.auto.tfvars
+++
@@ -1,6 +0,0 @@
-# 10_common_values.auto.tfvars
-
-location   = "eastus2"
-rgLzName   = "rg-use2-391575-s3-akswincont-avm-lz"
-vnetLzName = "vnet-lz"
-
--- azure/terraform/aks-lz-accelerator/10-GitLab-Self-managed/10_common_variables.tf
+++
@@ -1,7 +0,0 @@
-# 10_common_variables.tf
-
-variable "tenant_id" {}
-variable "subscription_id" {}
-variable "client_id" {}
-variable "client_secret" {}
-

d38ef20b53190085e9315628ef3a76572d7226ba
Emmanuel <bergerat62@gmail.com>
2025-03-05 12:15:04 -0500

    Updated connection strings saved in Key vault

--- azure/terraform/aks-lz-accelerator/08-AzSQL-Data/main.tf
+++ azure/terraform/aks-lz-accelerator/08-AzSQL-Data/main.tf
@@ -74,7 +74,7 @@ resource "azurerm_key_vault_secret" "az_sql_conn_string" {
   for_each = local.databases
 
   name         = replace("AzSql-Db-ConnectionString-${each.value.name}", "_", "-")
-  value        = "Server=tcp:${module.naming.sql_server.name_unique}.database.windows.net,1433;Initial Catalog=${each.value.name};Authentication=Active Directory Managed Identity;User Id=${module.avm-res-managedidentity-userassignedidentity.client_id};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+  value        = "Server=tcp:${module.naming.sql_server.name_unique}.database.windows.net,1433;Initial Catalog=${each.value.name};Persist Security Info=False;User ID=${var.sql_admin_username};Password=${random_password.sql_password.result};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=True;Connection Timeout=30;"
   key_vault_id = local.akvId
 
   tags = merge(var.base_tags, var.plan_tags)

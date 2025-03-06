a83b328d3815ca631f6fa7fb90b22f78465bc644
welasco <26201510+Welasco@users.noreply.github.com>
2025-03-06 10:40:58 -0600

    Fix environment variable timetracker

--- src/TimeTracker/entrypoint.ps1
+++ src/TimeTracker/entrypoint.ps1
@@ -1,3 +1,3 @@
-(Get-Content -Raw C:\inetpub\wwwroot\TimeTracker\web.config) -replace '###ReplaceTimeTrackerConnectionString###', $env:ConnectionString | Set-Content -NoNewLine C:\inetpub\wwwroot\TimeTracker\web.config
+(Get-Content -Raw C:\inetpub\wwwroot\TimeTracker\web.config) -replace '###ReplaceTimeTrackerConnectionString###', $env:ConnectionStringTimeTracker | Set-Content -NoNewLine C:\inetpub\wwwroot\TimeTracker\web.config
 #Remove-Item -Path Env:ConnectionString
 C:\ServiceMonitor.exe w3svc

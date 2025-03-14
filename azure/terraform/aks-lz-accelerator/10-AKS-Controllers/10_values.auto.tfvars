0838dd7718e236a27752f2ca9580f6c7c1132a00
Emmanuel <bergerat62@gmail.com>
2025-03-14 16:30:58 -0400

    Starting AKS-Controllers

---
+++ azure/terraform/aks-lz-accelerator/10-AKS-Controllers/10_values.auto.tfvars
@@ -0,0 +1,8 @@
+# 09_values.auto.tfvars
+
+plan_tags = {
+  Plan = "09-GitLab-Runner"
+}
+
+private_dns_zone_name = "private.contoso.com"
+
---
+++ azure/terraform/aks-lz-accelerator/10-AKS-Controllers/variables.tf
@@ -0,0 +1,14 @@
+variable "rgHubName" {
+  type = string
+}
+
+variable "rgLzName" {
+  type = string
+}
+
+variable "location" {
+  type    = string
+  default = "eastus2"
+}
+
+variable "private_dns_zone_name" {}

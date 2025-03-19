ba7cb7d32669b656a07a513668b984d69c0b5e4f
Emm <emberger@microsoft.com>
2025-03-18 21:55:19 -0400

    Commented nodepools os_disk_sizes

--- azure/terraform/aks-lz-accelerator/06-AKS-Cluster/main.tf
+++ azure/terraform/aks-lz-accelerator/06-AKS-Cluster/main.tf
@@ -79,9 +79,9 @@ resource "azurerm_kubernetes_cluster" "aks-cluster" {
   }
 
   default_node_pool {
-    name                         = "system"
-    vm_size                      = "Standard_DS2_v2"
-    os_disk_size_gb              = 30
+    name    = "system"
+    vm_size = "Standard_DS2_v2"
+    # os_disk_size_gb              = 30
     os_sku                       = "Ubuntu"
     min_count                    = 1
     max_count                    = 2
@@ -133,16 +133,16 @@ resource "azurerm_kubernetes_cluster_node_pool" "nodepool" {
   name                  = "ubu"
   kubernetes_cluster_id = azurerm_kubernetes_cluster.aks-cluster.id
   vm_size               = "Standard_DS2_v2"
-  os_disk_size_gb       = 30
-  os_type               = "Linux"
-  os_sku                = "Ubuntu"
-  min_count             = 2
-  max_count             = 3
-  enable_auto_scaling   = true
-  max_pods              = 250
-  mode                  = "User"
-  vnet_subnet_id        = local.snetAksId
-  zones                 = ["1", "3"] # zones = ["1", "2", "3"]
+  # os_disk_size_gb       = 30
+  os_type             = "Linux"
+  os_sku              = "Ubuntu"
+  min_count           = 2
+  max_count           = 3
+  enable_auto_scaling = true
+  max_pods            = 250
+  mode                = "User"
+  vnet_subnet_id      = local.snetAksId
+  zones               = ["1", "3"] # zones = ["1", "2", "3"]
 
   tags = merge(var.base_tags, var.plan_tags)
 }
